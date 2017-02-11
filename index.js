44675f806c7a4cf549fd72b870f4aa56fd3734bf
nonbeing <anattaspace@gmail.com>
2017-02-11 15:11:52 +0530

    don't need nicks[] in server

--- index.js
+++ index.js
@@ -6,30 +6,33 @@ const io = require('socket.io')(http);
 app.use(express.static('public'));
 
 var path_history = {}
-var nicks = []
+// var nicks = []
 
 io.on('connection', function(socket){
+  console.log("New connection, sending path_history: " + path_history)
+  socket.emit('path', path_history)
+
+
   socket.on('chat message', function(msg){
-    if (nicks.indexOf(msg.nick) == -1) {
-      nicks.push(msg.nick)
+    // if (nicks.indexOf(msg.nick) == -1) {
+    //   nicks.push(msg.nick)
 
-      console.log(msg.nick + " just connected for the first time, sending path_history: " + path_history)
-      socket.emit('chat message', path_history)
-    }
-
+    //   console.log(msg.nick + " just connected for the first time, sending path_history: " + path_history)
+    //   socket.emit('chat message', path_history)
+    // }
     console.log('msg: "' + msg.content + '", from: ' + msg.nick);
     io.emit('chat message', msg);
   });
 
   socket.on('path', function(data) {
-    if (nicks.indexOf(data.nick) == -1) {
-      nicks.push(data.nick)
+    // if (nicks.indexOf(data.nick) == -1) {
+    //   nicks.push(data.nick)
 
-      console.log(data.nick + " just connected for the first time, sending path_history: " + path_history)
-      socket.emit('chat message', path_history)
-    }
+    //   console.log(data.nick + " just connected for the first time, sending path_history: " + path_history)
+    //   socket.emit('path', path_history)
+    // }
 
-    console.log(data.name, data.point);
+    console.log("data.name:"+ data.name + " data.point:" + data.point);
 
     if(!path_history[data.name]) {
       path_history[data.name] = {"points":[], "color":data.color}
